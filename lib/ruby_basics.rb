b667a04a5b92c80cbd21105aeeebff1f970f482c
sensible-proc-7557 <vchan313@gmail.com>
2018-10-30 01:29:09 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,8 +10,8 @@ def argue(phrase="I'm right and you are wrong!")
   return phrase
 end
 
-def greeting(greeting = "Hi there, ",name = "Bobby")
-  
+def greeting(greeting = "Hi there, ",name = "Bob")
+  return greeting + name
 end
 
 def return_a_value
